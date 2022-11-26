cc722e1ec46bbebb9f781622c790235c07ba09c9
Natan Cardoso <natansc73@gmail.com>
2022-11-26 17:26:33 -0300

    Create Checkbox components

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,16 @@
+import { CheckboxContainer, CheckboxIndicator } from './styles'
+import { ComponentProps } from 'react'
+import { Check } from 'phosphor-react'
+
+// eslint-disable-next-line prettier/prettier
+export interface CheckboxProps extends ComponentProps<typeof CheckboxContainer> { }
+
+export function Checkbox() {
+  return (
+    <CheckboxContainer>
+      <CheckboxIndicator>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
