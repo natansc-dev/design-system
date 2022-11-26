a587f34d3439d53a614dfa27b5261a44b3c0d53c
Natan Cardoso <natansc73@gmail.com>
2022-11-26 16:28:18 -0300

    Create TextInput, storybook with decorators

---
+++ packages/react/src/components/TextInput/index.tsx
@@ -0,0 +1,16 @@
+import { Input, Prefix, TextInputContainer } from './styles'
+import { ComponentProps } from 'react'
+
+export interface TextInputProps extends ComponentProps<typeof Input> {
+  prefix?: string
+}
+
+export function TextInput({ prefix, ...props }: TextInputProps) {
+  return (
+    <TextInputContainer>
+      {!!prefix && <Prefix>cal.com/</Prefix>}
+
+      <Input {...props} />
+    </TextInputContainer>
+  )
+}
